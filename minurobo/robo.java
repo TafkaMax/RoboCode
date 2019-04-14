8907ad38633bf6bb4fd1e4f5a88eb37ca6900064
Annabel <antsar@ttu.ee>
2019-04-14 21:18:47 +0300

    anni robo

---
+++ minurobo/robo.java
@@ -0,0 +1,33 @@
+package RoboCode.minurobo;
+
+import robocode.HitByBulletEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class robo extends Robot {
+
+    public void run() {
+
+        while (true) {
+            ahead(100); // Move ahead 100
+            turnGunRight(360); // Spin gun around
+            back(100); // Move back 100
+            turnGunRight(360); // Spin gun around
+        }
+    }
+
+    /**
+     * Fire when we see a robot
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    /**
+     * We were hit!  Turn perpendicular to the bullet,
+     * so our seesaw might avoid a future shot.
+     */
+    public void onHitByBullet(HitByBulletEvent e) {
+        turnLeft(90 - e.getBearing());
+    }
+}
